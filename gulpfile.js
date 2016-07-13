c9663786fb31cf7829fa3f8f2787311ad221b126
kirill <kirill.gus@gmail.com>
2016-07-13 16:19:29 +0300

    Initial commit for lesson of npm and gulp

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+  var sass = require('gulp-sass');
+  return gulp.src('./styles/*.sass')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('styles.css'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./compiled'));
+});
+
+gulp.task('scripts', function () {
+  var es6transpiler = require('gulp-es6-transpiler'),
+      uglify = require('gulp-uglify');
+  return gulp.src('./scripts/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(es6transpiler())
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./compiled'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./styles/*.sass', ['sass']);
+  gulp.watch('./scripts/*.js', ['scripts']);
+});
---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+function testing() {
+    let variable = 2;
+    console.log(variable);
+}
+
+testing();
